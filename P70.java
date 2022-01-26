638c63db4c4e19844f7c3af1921b72e8162a0510
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-01-26 16:04:43 +0800

    Update P70.java

--- P70.java
+++ P70.java
@@ -1,4 +1,24 @@
 package leccod;
 
+import jdk.internal.org.objectweb.asm.tree.MultiANewArrayInsnNode;
+
 public class P70 {
+    public static void main(String[] args) {
+        System.out.println(new P70().climbStairs(45));
+    }
+
+
+    public int climbStairs(int n) {
+        if (n == 1 || n== 2){
+            return n;
+        }
+        int[] arr = new int[n+1];
+        arr[1] =1;arr[2] =2;
+
+        for (int i = 3; i <= n ; i++) {
+            arr[i] = arr[i-1]+ arr[i-2];
+        }
+
+        return arr[n];
+    }
 }
