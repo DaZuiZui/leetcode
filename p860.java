8e8cda2f9fb7661dbae56aaf49b4917a4138207e
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-04-03 22:19:09 +0800

    Add files via upload

---
+++ p860.java
@@ -0,0 +1,34 @@
+package leccod;
+
+public class p860 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten  = 0;
+        for (int bill : bills) {
+            //如果收到5元
+            if (bill == 5){
+                five++;
+            }
+            //如果收到10元
+            else if (bill == 10){
+                five--;
+                ten++;
+            }
+            //如果收到20元
+            else if(bill == 20){
+                //查看能用十元不
+                if (ten > 0){
+                    ten--;
+                    five--;
+                }else{
+                    five-=3;
+                }
+            }
+        }
+        return five >= 0 ? true : false;
+    }
+}
