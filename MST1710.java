04e69d0668e0b5762a3e1fabfa28e207391e88f3
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-04-03 12:31:44 +0800

    Add files via upload

--- MST1710.java
+++ MST1710.java
@@ -1,13 +1,25 @@
 package leccod;
 
+import java.util.HashMap;
+
 public class MST1710 {
     public static void main(String[] args) {
 
     }
-
     public int majorityElement(int[] nums) {
+        int mid = nums.length/2;
 
-        return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        for (Integer integer : map.keySet()) {
+            if (map.get(integer) > mid){
+                return map.get(integer);
+            }
+        }
+
+        return -1;
     }
-
 }
