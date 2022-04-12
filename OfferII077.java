4e4a9787f31a6eecab1c91dbbd134e978a98c2c9
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-04-12 15:42:01 +0800

    Add files via upload

---
+++ OfferII077.java
@@ -0,0 +1,37 @@
+package leccod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OfferII077 {
+    public ListNode sortList(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode temp = head;
+        List<Integer> list = new ArrayList<>();
+        while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
+        }
+        list.sort(null);
+        ListNode res = new ListNode(list.get(0));
+        if(list.size() > 1){
+            res.next = new ListNode();
+            temp = res.next;
+        }
+
+
+        for(int i = 1 ; i < list.size() ; i++){
+            temp.val = list.get(i);
+            if(i!= list.size()-1){
+                temp.next = new ListNode();
+                temp = temp.next;
+            }
+        }
+
+
+
+        return res;
+    }
+}
