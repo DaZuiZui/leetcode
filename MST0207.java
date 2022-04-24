000647ca2a4ac4857ea786feaf64275259935e4f
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-04-24 15:52:39 +0800

    Add files via upload

---
+++ MST0207.java
@@ -0,0 +1,27 @@
+package leccod;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MST0207 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+
+        while (headA != null){
+            set.add(headA);
+            headA = headA.next;
+        }
+
+        while(headB != null){
+            if (set.contains(headB)){
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
