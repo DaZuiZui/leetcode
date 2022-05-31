d6101ac1821cffc456c7dbbbdc42505c1dc2f492
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-05-31 14:46:38 +0800

    Add files via upload

---
+++ P1108.java
@@ -0,0 +1,11 @@
+package leccod;
+
+public class P1108 {
+    public static void main(String[] args) {
+
+    }
+
+    public String defangIPaddr(String address) {
+        return address.replace(".","[.]");
+    }
+}
