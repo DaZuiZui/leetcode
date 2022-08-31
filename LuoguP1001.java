28f28f02db1dbeba1cb49713a4cd77ffed991ab7
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-08-31 11:28:05 +0800

    Add files via upload

---
+++ LuoguP1001.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class LuoguP1001 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        System.out.println(a+b);
+    }
+}
