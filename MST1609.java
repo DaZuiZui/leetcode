2f700fc590ff8ecceae92de751fc28584d38312a
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-11-17 23:44:00 +0800

    Add files via upload

---
+++ MST1609.java
@@ -0,0 +1,21 @@
+public class MST1609 {
+}
+
+class Operations {
+
+    public Operations() {
+
+    }
+
+    public int minus(int a, int b) {
+        return  a-b;
+    }
+
+    public int multiply(int a, int b) {
+        return a*b;
+    }
+
+    public int divide(int a, int b) {
+        return a/b;
+    }
+}
