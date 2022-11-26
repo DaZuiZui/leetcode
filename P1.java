e384f1207397251d5a8bf5328319bc60534174c6
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2022-11-26 22:11:28 +0800

    Add files via upload

--- P1.java
+++ P1.java
@@ -1,21 +1,11 @@
-package leccod;
+package chuanzhibei;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class P1 {
     public static void main(String[] args) {
-
-    }
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap();
-
-        for(int i = 0 ; i < nums.length ; i++){
-            if(map.containsKey(target - nums[i])){
-                return new int[] {map.get(target - nums[i]),i};
-            }
-            map.put(nums[i],i);
-        }
-        return new int[]{0,0};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(2%3);
+        scanner.close();
     }
 }
