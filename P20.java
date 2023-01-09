83f819102b89575350e2411b849b68a5c439ee06
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-01-09 22:05:08 +0800

    Add files via upload

--- P20.java
+++ P20.java
@@ -1,4 +1,4 @@
-package leccod;
+import java.util.Stack;
 
 public class P20 {
     public static void main(String[] args) {
@@ -6,12 +6,18 @@ public class P20 {
     }
 
     public boolean isValid(String s) {
-        int length = s.length() /2;
-
-        for (int i = 0; i < length; i++) {
-            s = s.replace("()","").replace("[]","").replace("{}","");
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '{') stack.push('}');
+            else if (c == '(') stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c){
+                return false;
+            }
         }
 
-        return s.length() == 0;
+        return stack.isEmpty();
     }
 }
