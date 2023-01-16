586b992ab8db7f49f56f327326f1bd1dbe4b0692
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-01-17 01:05:07 +0800

    Add files via upload

---
+++ day3/P343.java
@@ -0,0 +1,19 @@
+package day3;
+
+public class P343 {
+    public static void main(String[] args) {
+
+    }
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] =1;
+        for (int i = 2; i <=n ; i++) {
+            for (int j = 0; j <= i; j++) {
+                dp[i] += dp[j-1] * dp[i-j];
+            }
+        }
+
+        return dp[n];
+    }
+}
