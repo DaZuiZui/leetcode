141e8a3ac20a0543c6116ac9713ef0f5d6f9c500
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-01-22 18:35:45 +0800

    Add files via upload

---
+++ LCP377.java
@@ -0,0 +1,17 @@
+public class LCP377 {
+    public static void main(String[] args) {
+
+    }
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target+1];
+        dp[0] =1;
+
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (i-nums[i] >=0)dp[i] += dp[i - nums[j]];
+            }
+        }
+        return dp[target];
+    }
+}
