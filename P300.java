f8d6ca27a18c94163cc78ae0862cfe3ca82bfbe8
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-01-31 21:44:05 +0800

    Add files via upload

--- P300.java
+++ P300.java
@@ -1,27 +1,24 @@
-package leccod;
+import java.util.Arrays;
 
 public class P300 {
     public static void main(String[] args) {
-
+        System.err.println(new P300().lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
     }
 
     public int lengthOfLIS(int[] nums) {
-        if (nums.length == 0){
-            return 0;
-        }
-        int res = 1;
         int[] dp = new int[nums.length];
-        dp[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            dp[i] = 1;
+        Arrays.fill(dp,1);
+        int n = nums.length;
+        int res = 1;
+        //便利物品
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                    dp[i] = Math.max(dp[i],dp[j]+1);
                 }
             }
-            res = Math.max(res, dp[i]);
+            res = Math.max(res,dp[i]);
         }
-
         return res;
     }
 }
