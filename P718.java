697d2e98a5853146562369f3044bdfcd94be25c5
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-02-01 00:10:29 +0800

    Add files via upload

---
+++ P718.java
@@ -0,0 +1,26 @@
+public class P718 {
+    public static void main(String[] args) {
+        System.err.println(new P718().findLength(
+                new int[]{1, 2, 3, 2, 1},
+                new int[]{3, 2, 1, 4, 7}
+        ));
+    }
+
+    public int findLength(int[] nums1, int[] nums2) {
+        int res =0;
+        int n = nums1.length;
+        int m = nums2.length;
+        int[][] dp = new int[n+1][m+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m ; j++) {
+                if (nums1[i-1] == nums2[j-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                    res = Math.max(dp[i][j],res);
+                }
+            }
+        }
+
+        return res;
+    }
+}
