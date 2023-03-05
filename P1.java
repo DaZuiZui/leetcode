2933676838d1efecd6dddcee28fdb68004b92221
哇塞大嘴好帥 <66861267+DaZuiZui@users.noreply.github.com>
2023-03-05 21:40:11 +0800

    Add files via upload

--- P1.java
+++ P1.java
@@ -1,11 +1,26 @@
-package chuanzhibei;
-
-import java.util.Scanner;
+package weekly335;
 
 public class P1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(2%3);
-        scanner.close();
+        System.out.println(new P1().passThePillow(18, 38));
+    }
+
+    public int passThePillow(int n, int time) {
+        boolean b = true;
+        int res = 1;
+
+        for (int i = 1; i <= time; i++) {
+            if (res  == n){
+                b = false;
+            }else if (res == 1){
+                b = true;
+            }
+            if (b){
+                res++;
+            }else{
+                res--;
+            }
+        }
+        return res;
     }
 }
